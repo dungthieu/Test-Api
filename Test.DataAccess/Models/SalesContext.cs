19ee0530501be1dff566bd855489353da317c8d4
dung thieu <hvhust1998@gmail.com>
2020-12-29 15:55:49 +0700

    update

--- Test.DataAccess/Models/SalesContext.cs
+++ Test.DataAccess/Models/SalesContext.cs
@@ -14,33 +14,16 @@ namespace Test.DataAccess.Models
             : base(options)
         {
         }
-
-        public virtual DbSet<AlphabeticalListOfProduct> AlphabeticalListOfProducts { get; set; }
         public virtual DbSet<Cart> Carts { get; set; }
         public virtual DbSet<Category> Categories { get; set; }
-        public virtual DbSet<CategorySalesFor1997> CategorySalesFor1997s { get; set; }
-        public virtual DbSet<CurrentProductList> CurrentProductLists { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
-        public virtual DbSet<CustomerAndSuppliersByCity> CustomerAndSuppliersByCities { get; set; }
         public virtual DbSet<Employee> Employees { get; set; }
         public virtual DbSet<EmployeeTerritory> EmployeeTerritories { get; set; }
-        public virtual DbSet<Invoice> Invoices { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
         public virtual DbSet<OrderDetail> OrderDetails { get; set; }
-        public virtual DbSet<OrderDetailsExtended> OrderDetailsExtendeds { get; set; }
-        public virtual DbSet<OrderSubtotal> OrderSubtotals { get; set; }
-        public virtual DbSet<OrdersQry> OrdersQries { get; set; }
         public virtual DbSet<Product> Products { get; set; }
-        public virtual DbSet<ProductSalesFor1997> ProductSalesFor1997s { get; set; }
-        public virtual DbSet<ProductsAboveAveragePrice> ProductsAboveAveragePrices { get; set; }
-        public virtual DbSet<ProductsByCategory> ProductsByCategories { get; set; }
-        public virtual DbSet<QuarterlyOrder> QuarterlyOrders { get; set; }
         public virtual DbSet<Region> Regions { get; set; }
-        public virtual DbSet<SalesByCategory> SalesByCategories { get; set; }
-        public virtual DbSet<SalesTotalsByAmount> SalesTotalsByAmounts { get; set; }
         public virtual DbSet<Shipper> Shippers { get; set; }
-        public virtual DbSet<SummaryOfSalesByQuarter> SummaryOfSalesByQuarters { get; set; }
-        public virtual DbSet<SummaryOfSalesByYear> SummaryOfSalesByYears { get; set; }
         public virtual DbSet<Supplier> Suppliers { get; set; }
         public virtual DbSet<Territory> Territories { get; set; }
 
@@ -55,31 +38,6 @@ namespace Test.DataAccess.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
-
-            modelBuilder.Entity<AlphabeticalListOfProduct>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Alphabetical list of products");
-
-                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
-
-                entity.Property(e => e.CategoryName)
-                    .IsRequired()
-                    .HasMaxLength(15);
-
-                entity.Property(e => e.ProductId).HasColumnName("ProductID");
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
-
-                entity.Property(e => e.SupplierId).HasColumnName("SupplierID");
-
-                entity.Property(e => e.UnitPrice).HasColumnType("money");
-            });
 
             modelBuilder.Entity<Cart>(entity =>
             {
@@ -128,34 +86,6 @@ namespace Test.DataAccess.Models
                 entity.Property(e => e.Picture).HasColumnType("image");
             });
 
-            modelBuilder.Entity<CategorySalesFor1997>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Category Sales for 1997");
-
-                entity.Property(e => e.CategoryName)
-                    .IsRequired()
-                    .HasMaxLength(15);
-
-                entity.Property(e => e.CategorySales).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<CurrentProductList>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Current Product List");
-
-                entity.Property(e => e.ProductId)
-                    .ValueGeneratedOnAdd()
-                    .HasColumnName("ProductID");
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-            });
-
             modelBuilder.Entity<Customer>(entity =>
             {
                 entity.Property(e => e.CustomerId)
@@ -188,26 +118,6 @@ namespace Test.DataAccess.Models
                 entity.Property(e => e.Region).HasMaxLength(15);
 
                 entity.Property(e => e.User).HasMaxLength(10);
-            });
-
-            modelBuilder.Entity<CustomerAndSuppliersByCity>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Customer and Suppliers by City");
-
-                entity.Property(e => e.City).HasMaxLength(15);
-
-                entity.Property(e => e.CompanyName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.ContactName).HasMaxLength(30);
-
-                entity.Property(e => e.Relationship)
-                    .IsRequired()
-                    .HasMaxLength(9)
-                    .IsUnicode(false);
             });
 
             modelBuilder.Entity<Employee>(entity =>
@@ -275,71 +185,6 @@ namespace Test.DataAccess.Models
                     .HasConstraintName("FK_EmployeeTerritories_Territories");
             });
 
-            modelBuilder.Entity<Invoice>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Invoices");
-
-                entity.Property(e => e.Address).HasMaxLength(60);
-
-                entity.Property(e => e.City).HasMaxLength(15);
-
-                entity.Property(e => e.Country).HasMaxLength(15);
-
-                entity.Property(e => e.CustomerId)
-                    .HasMaxLength(5)
-                    .HasColumnName("CustomerID")
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.CustomerName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.ExtendedPrice).HasColumnType("money");
-
-                entity.Property(e => e.Freight).HasColumnType("money");
-
-                entity.Property(e => e.OrderDate).HasColumnType("datetime");
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.PostalCode).HasMaxLength(10);
-
-                entity.Property(e => e.ProductId).HasColumnName("ProductID");
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.Region).HasMaxLength(15);
-
-                entity.Property(e => e.RequiredDate).HasColumnType("datetime");
-
-                entity.Property(e => e.Salesperson)
-                    .IsRequired()
-                    .HasMaxLength(31);
-
-                entity.Property(e => e.ShipAddress).HasMaxLength(60);
-
-                entity.Property(e => e.ShipCity).HasMaxLength(15);
-
-                entity.Property(e => e.ShipCountry).HasMaxLength(15);
-
-                entity.Property(e => e.ShipName).HasMaxLength(40);
-
-                entity.Property(e => e.ShipPostalCode).HasMaxLength(10);
-
-                entity.Property(e => e.ShipRegion).HasMaxLength(15);
-
-                entity.Property(e => e.ShippedDate).HasColumnType("datetime");
-
-                entity.Property(e => e.ShipperName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.UnitPrice).HasColumnType("money");
-            });
 
             modelBuilder.Entity<Order>(entity =>
             {
@@ -420,86 +265,6 @@ namespace Test.DataAccess.Models
                     .HasConstraintName("FK_Order_Details_Products");
             });
 
-            modelBuilder.Entity<OrderDetailsExtended>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Order Details Extended");
-
-                entity.Property(e => e.ExtendedPrice).HasColumnType("money");
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.ProductId).HasColumnName("ProductID");
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.UnitPrice).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<OrderSubtotal>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Order Subtotals");
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.Subtotal).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<OrdersQry>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Orders Qry");
-
-                entity.Property(e => e.Address).HasMaxLength(60);
-
-                entity.Property(e => e.City).HasMaxLength(15);
-
-                entity.Property(e => e.CompanyName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.Country).HasMaxLength(15);
-
-                entity.Property(e => e.CustomerId)
-                    .HasMaxLength(5)
-                    .HasColumnName("CustomerID")
-                    .IsFixedLength(true);
-
-                entity.Property(e => e.EmployeeId).HasColumnName("EmployeeID");
-
-                entity.Property(e => e.Freight).HasColumnType("money");
-
-                entity.Property(e => e.OrderDate).HasColumnType("datetime");
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.PostalCode).HasMaxLength(10);
-
-                entity.Property(e => e.Region).HasMaxLength(15);
-
-                entity.Property(e => e.RequiredDate).HasColumnType("datetime");
-
-                entity.Property(e => e.ShipAddress).HasMaxLength(60);
-
-                entity.Property(e => e.ShipCity).HasMaxLength(15);
-
-                entity.Property(e => e.ShipCountry).HasMaxLength(15);
-
-                entity.Property(e => e.ShipName).HasMaxLength(40);
-
-                entity.Property(e => e.ShipPostalCode).HasMaxLength(10);
-
-                entity.Property(e => e.ShipRegion).HasMaxLength(15);
-
-                entity.Property(e => e.ShippedDate).HasColumnType("datetime");
-            });
-
             modelBuilder.Entity<Product>(entity =>
             {
                 entity.Property(e => e.ProductId).HasColumnName("ProductID");
@@ -535,71 +300,6 @@ namespace Test.DataAccess.Models
                     .HasConstraintName("FK_Products_Suppliers");
             });
 
-            modelBuilder.Entity<ProductSalesFor1997>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Product Sales for 1997");
-
-                entity.Property(e => e.CategoryName)
-                    .IsRequired()
-                    .HasMaxLength(15);
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.ProductSales).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<ProductsAboveAveragePrice>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Products Above Average Price");
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.UnitPrice).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<ProductsByCategory>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Products by Category");
-
-                entity.Property(e => e.CategoryName)
-                    .IsRequired()
-                    .HasMaxLength(15);
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.QuantityPerUnit).HasMaxLength(20);
-            });
-
-            modelBuilder.Entity<QuarterlyOrder>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Quarterly Orders");
-
-                entity.Property(e => e.City).HasMaxLength(15);
-
-                entity.Property(e => e.CompanyName).HasMaxLength(40);
-
-                entity.Property(e => e.Country).HasMaxLength(15);
-
-                entity.Property(e => e.CustomerId)
-                    .HasMaxLength(5)
-                    .HasColumnName("CustomerID")
-                    .IsFixedLength(true);
-            });
-
             modelBuilder.Entity<Region>(entity =>
             {
                 entity.HasKey(e => e.RegionId)
@@ -617,42 +317,6 @@ namespace Test.DataAccess.Models
                     .IsFixedLength(true);
             });
 
-            modelBuilder.Entity<SalesByCategory>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Sales by Category");
-
-                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
-
-                entity.Property(e => e.CategoryName)
-                    .IsRequired()
-                    .HasMaxLength(15);
-
-                entity.Property(e => e.ProductName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.ProductSales).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<SalesTotalsByAmount>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Sales Totals by Amount");
-
-                entity.Property(e => e.CompanyName)
-                    .IsRequired()
-                    .HasMaxLength(40);
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.SaleAmount).HasColumnType("money");
-
-                entity.Property(e => e.ShippedDate).HasColumnType("datetime");
-            });
-
             modelBuilder.Entity<Shipper>(entity =>
             {
                 entity.Property(e => e.ShipperId).HasColumnName("ShipperID");
@@ -662,32 +326,6 @@ namespace Test.DataAccess.Models
                     .HasMaxLength(40);
 
                 entity.Property(e => e.Phone).HasMaxLength(24);
-            });
-
-            modelBuilder.Entity<SummaryOfSalesByQuarter>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Summary of Sales by Quarter");
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.ShippedDate).HasColumnType("datetime");
-
-                entity.Property(e => e.Subtotal).HasColumnType("money");
-            });
-
-            modelBuilder.Entity<SummaryOfSalesByYear>(entity =>
-            {
-                entity.HasNoKey();
-
-                entity.ToView("Summary of Sales by Year");
-
-                entity.Property(e => e.OrderId).HasColumnName("OrderID");
-
-                entity.Property(e => e.ShippedDate).HasColumnType("datetime");
-
-                entity.Property(e => e.Subtotal).HasColumnType("money");
             });
 
             modelBuilder.Entity<Supplier>(entity =>
